955d966c20c79757b60fda9bb4e277af87f6e560
aakriti poudel <poudelaakritisimmi@gmail.com>
2024-01-25 20:05:41 +0530

    github profile exploration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { SearchPageComponent } from './components/search-page/search-page.component';
+import { UserDetailsComponent } from './components/user-details/user-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'search',
+    component: SearchPageComponent
+  },
+  {
+    path: 'user/:id',
+    component: UserDetailsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
